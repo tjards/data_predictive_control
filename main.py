83f4fa7a0a2e91dca95f6e4db7a0d34506748cef
tjards <p.jardine@queensu.ca>
2026-06-28 20:24:57 -0400

    built augmented matrix

--- main.py
+++ main.py
@@ -1,5 +1,29 @@
 
 # standard inputs
+import numpy as np
+import json
 
 # custom imports
 import mpc 
+
+
+# Load parameters from config
+with open('config.json') as f:
+    cfg = json.load(f)
+
+# load parameters from config
+A   = np.array(cfg['A'])
+B   = np.array(cfg['B'])
+Q   = np.diag(cfg['Q_diag'])
+R   = np.diag(cfg['R_diag'])
+P   = np.diag(cfg['P_diag'])
+x0  = np.array(cfg['x0'])
+u0  = np.array(cfg['u0'])
+h   = cfg['h']
+m   = cfg['m']
+constraints = cfg['constraints']
+
+# initialize the MPC controller
+mpc = mpc.MPC(A, B, Q, R, P, x0, u0, h, m, constraints)
+
+
--- mpc.py
+++ mpc.py
@@ -1,9 +1,20 @@
 '''
 Implementation of convex Model Predictive Control (MPC)
 
-Stardard Form:
+Standard Form:
 
 x(k+1) = Ax(k) + Bu(k)
+
+Constraints:
+
+    x_min <= x(k) <= x_max
+    u_min <= u(k) <= u_max
+
+    or 
+
+    Mx <= bx
+    Mu <= bu
+
 '''
 
 import numpy as np
@@ -25,4 +36,39 @@ class MPC():
         self.nx = self.A.shape[0]               # state dimensions
         self.nu = self.B.shape[1]               # input dimensions
         self.constraints = constraints
+        self._define_constraints()
+        self._build_augmented_matrices()
+
+    def _define_constraints(self):
+
+        if self.constraints['type'] == 'box':
+            self.x_min = np.array(self.constraints['x_min']).reshape(-1, 1)
+            self.x_max = np.array(self.constraints['x_max']).reshape(-1, 1)
+            self.u_min = np.array(self.constraints['u_min']).reshape(-1, 1)
+            self.u_max = np.array(self.constraints['u_max']).reshape(-1, 1)
+        elif self.constraints['type'] == 'lmi':
+            self.Mx = np.array(self.constraints['Mx'], ndmin=2)
+            self.bx = np.array(self.constraints['bx'], ndmin=2)
+            self.Mu = np.array(self.constraints['Mu'], ndmin=2)
+            self.bu = np.array(self.constraints['bu'], ndmin=2)
+        else:
+            raise ValueError("Invalid constraint type: Must be 'box' or 'lmi'")
+        
+    def _build_augmented_matrices(self):
+
+        # initialize the augmented matrices over prediction horizon
+        self.A_aug = np.zeros((self.nx * self.h, self.nx))
+        self.B_aug = np.zeros((self.nx * self.h, self.nu * self.h))
+
+        # build the augmented matrices
+        for i in range(self.h):
+            self.A_aug[i*self.nx:(i+1)*self.nx, :] = np.linalg.matrix_power(self.A, i+1)
+            for j in range(i+1):
+                self.B_aug[i*self.nx:(i+1)*self.nx, j*self.nu:(j+1)*self.nu] = np.linalg.matrix_power(self.A, i-j) @ self.B
+
+
+    
+
+
+
 
